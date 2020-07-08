538413ae720853904c1e45b619c4d05d9425f0f7
lm <710450868@qq.com>
2020-07-08 11:52:57 +0800

    完成toast测试用例

--- src/app.js
+++ src/app.js
@@ -58,8 +58,8 @@ new Vue({
              console.log('他说已经充值智商了')
            }
          },
-         autoClose: false,
-         autoCloseDelay: 3
+         autoClose: 3,
+
        })
      }
   }
