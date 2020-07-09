ad8fc6da1932b2e064a6834e41b64a97878920a9
lm <710450868@qq.com>
2020-07-09 09:52:01 +0800

    tab切换动画完成

--- src/tabs-head.vue
+++ src/tabs-head.vue
@@ -11,9 +11,11 @@
   export default {
     name:'GuluTabsHead',
     inject: ['eventBus'],
-    created () {
+    mounted () {
        this.eventBus.$on('update:selected',(item,vm)=>{
-         console.log(vm.$el)
+         let {width, height, top, left} = vm.$el.getBoundingClientRect()
+         this.$refs.line.style.width = `${width}px`
+         this.$refs.line.style.left = `${left}px`
        })
     }
   }
@@ -26,13 +28,12 @@
     display: flex;
     height: $tab-height;
     justify-content: flex-start;
-    border: 1px solid red;
     position:relative;
     >.line{
       position:absolute;
       bottom:0;
       border-bottom: 1px solid $blue;
-      width: 100px;
+      transition: all 350ms;
     }
     > .actions-wrapper {
       margin-left: auto;
