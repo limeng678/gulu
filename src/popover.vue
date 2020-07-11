71658223c43c71b0c475e199f2534bad2c7e4b9d
lm <710450868@qq.com>
2020-07-11 09:25:44 +0800

    解决抖动问题

--- src/popover.vue
+++ src/popover.vue
@@ -150,10 +150,12 @@
         left: 10px;
       }
       &::before {
+        border-bottom: none;
         border-top-color: black;
         top: 100%;
       }
       &::after {
+        border-bottom: none;
         border-top-color: white;
         top: calc(100% - 1px);
       }
@@ -164,10 +166,12 @@
         left: 10px;
       }
       &::before {
+        border-top:none;
         border-bottom-color: black;
         bottom: 100%;
       }
       &::after {
+        border-top: none;
         border-bottom-color: white;
         bottom: calc(100% - 1px);
       }
@@ -180,10 +184,12 @@
         top: 50%;
       }
       &::before {
+        border-right:none;
         border-left-color: black;
         left: 100%;
       }
       &::after {
+        border-right: none;
         border-left-color: white;
         left: calc(100% - 1px);
       }
@@ -195,10 +201,12 @@
         top: 50%;
       }
       &::before {
+        border-left: none;
         border-right-color: black;
         right: 100%;
       }
       &::after {
+        border-left: none;
         border-right-color: white;
         right: calc(100% - 1px);
       }
